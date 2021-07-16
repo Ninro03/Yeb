c6f9546107781b540cdf311046180cd589f42736
wend2111 <chlqjagml730@gmail.com>
2021-07-16 17:42:07 +0900

    main add

--- js/html5div.js
+++
@@ -1,7 +0,0 @@
-﻿document.createElement('article');
-document.createElement('section');
-document.createElement('aside');
-document.createElement('hgroup');
-document.createElement('nav');
-document.createElement('header');
-document.createElement('footer');
