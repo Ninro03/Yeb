0f8d79ef04788fdc77d7a58137e2f7d109755f70
wend2111 <chlqjagml730@gmail.com>
2021-07-21 17:48:55 +0900

    map.html

--- js/script.js
+++ js/script.js
@@ -1,4 +1,19 @@
 ﻿$(function(){	
+	/* content1 bxslider */
+	$('.bxslider').bxSlider( {
+        mode: 'horizontal',// 가로 방향 수평 슬라이드
+        speed: 500,        // 이동 속도를 설정
+        pager: false,      // 현재 위치 페이징 표시 여부 설정
+        moveSlides: 3,     // 슬라이드 이동시 개수
+        slideWidth: 150,   // 슬라이드 너비
+        minSlides: 1,      // 최소 노출 개수
+        maxSlides: 4,      // 최대 노출 개수
+        slideMargin: 15,    // 슬라이드간의 간격
+        auto: true,        // 자동 실행 여부
+        autoHover: true,   // 마우스 호버시 정지 여부
+        controls: false    // 이전 다음 버튼 노출 여부
+    });
+	
 	/* 지역탭 */
 	var s_tab = $('.s_tab > ul > li');
 	var s_content = $('.match_list > ul');
